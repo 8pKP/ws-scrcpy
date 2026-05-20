ec77e6e279cbb6f16e32c45d01b8e4e8d5d02a8d
8pKP <138131820+8pKP@users.noreply.github.com>
2026-05-20 11:32:37 +0000

    feat: enhance touch layer by disabling touch actions

--- src/app/interactionHandler/SimpleInteractionHandler.ts
+++ src/app/interactionHandler/SimpleInteractionHandler.ts
@@ -2,6 +2,7 @@ import { InteractionEvents, InteractionHandler } from './InteractionHandler';
 import { BasePlayer } from '../player/BasePlayer';
 import ScreenInfo from '../ScreenInfo';
 import Position from '../Position';
+import { TouchControlMessage } from '../controlMessage/TouchControlMessage';
 
 export interface TouchHandlerListener {
     performClick: (position: Position) => void;
@@ -13,8 +14,17 @@ const TAG = '[SimpleTouchHandler]';
 export class SimpleInteractionHandler extends InteractionHandler {
     private startPosition?: Position;
     private endPosition?: Position;
-    private static readonly touchEventsNames: InteractionEvents[] = ['mousedown', 'mouseup', 'mousemove'];
-    private storage = new Map();
+    private static readonly touchEventsNames: InteractionEvents[] = [
+        'touchstart',
+        'touchend',
+        'touchmove',
+        'touchcancel',
+        'mousedown',
+        'mouseup',
+        'mousemove',
+    ];
+    private readonly storedFromMouseEvent = new Map<number, TouchControlMessage>();
+    private readonly storedFromTouchEvent = new Map<number, TouchControlMessage>();
 
     constructor(player: BasePlayer, private readonly listener: TouchHandlerListener) {
         super(player, SimpleInteractionHandler.touchEventsNames, []);
@@ -22,59 +32,75 @@ export class SimpleInteractionHandler extends InteractionHandler {
 
     protected onInteraction(event: MouseEvent | TouchEvent): void {
         let handled = false;
-        if (!(event instanceof MouseEvent)) {
+        if (event.target !== this.tag) {
             return;
         }
-        if (event.target === this.tag) {
-            const screenInfo: ScreenInfo = this.player.getScreenInfo() as ScreenInfo;
-            if (!screenInfo) {
-                return;
-            }
-            const events = this.buildTouchEvent(event, screenInfo, this.storage);
-            if (events.length > 1) {
-                console.warn(TAG, 'Too many events', events);
-                return;
-            }
-            const downEventName = 'mousedown';
-            if (events.length === 1) {
-                handled = true;
-                if (event.type === downEventName) {
-                    this.startPosition = events[0].position;
-                } else {
-                    if (this.startPosition) {
-                        this.endPosition = events[0].position;
-                    } else {
-                        console.warn(TAG, `Received "${event.type}" before "${downEventName}"`);
-                    }
-                }
+        const screenInfo: ScreenInfo = this.player.getScreenInfo() as ScreenInfo;
+        if (!screenInfo) {
+            return;
+        }
+        let events: TouchControlMessage[];
+        let downEventName: InteractionEvents;
+        let upEventName: InteractionEvents;
+        let cancelEventName: InteractionEvents | undefined;
+        if (event instanceof MouseEvent) {
+            events = this.buildTouchEvent(event, screenInfo, this.storedFromMouseEvent);
+            downEventName = 'mousedown';
+            upEventName = 'mouseup';
+        } else if (window['TouchEvent'] && event instanceof TouchEvent) {
+            events = this.formatTouchEvent(event, screenInfo, this.storedFromTouchEvent);
+            downEventName = 'touchstart';
+            upEventName = 'touchend';
+            cancelEventName = 'touchcancel';
+        } else {
+            console.error(TAG, 'Unsupported event', event);
+            return;
+        }
+        if (events.length > 1) {
+            console.warn(TAG, 'Too many events', events);
+            return;
+        }
+        if (events.length === 1) {
+            handled = true;
+            if (event.type === downEventName) {
+                this.startPosition = events[0].position;
+            } else {
                 if (this.startPosition) {
-                    this.drawPointer(this.startPosition.point);
-                }
-                if (this.endPosition) {
-                    this.drawPointer(this.endPosition.point);
-                    if (this.startPosition) {
-                        this.drawLine(this.startPosition.point, this.endPosition.point);
-                    }
-                }
-                if (event.type === 'mouseup') {
-                    if (this.startPosition && this.endPosition) {
-                        this.clearCanvas();
-                        if (this.startPosition.point.distance(this.endPosition.point) < 10) {
-                            this.listener.performClick(this.endPosition);
-                        } else {
-                            this.listener.performScroll(this.startPosition, this.endPosition);
-                        }
-                    }
+                    this.endPosition = events[0].position;
+                } else {
+                    console.warn(TAG, `Received "${event.type}" before "${downEventName}"`);
                 }
             }
-            if (handled) {
-                if (event.cancelable) {
-                    event.preventDefault();
+            if (this.startPosition) {
+                this.drawPointer(this.startPosition.point);
+            }
+            if (this.endPosition) {
+                this.drawPointer(this.endPosition.point);
+                if (this.startPosition) {
+                    this.drawLine(this.startPosition.point, this.endPosition.point);
                 }
-                event.stopPropagation();
+            }
+            if (event.type === upEventName) {
+                if (this.startPosition && this.endPosition) {
+                    this.clearCanvas();
+                    if (this.startPosition.point.distance(this.endPosition.point) < 10) {
+                        this.listener.performClick(this.endPosition);
+                    } else {
+                        this.listener.performScroll(this.startPosition, this.endPosition);
+                    }
+                }
             }
         }
-        if (event.type === 'mouseup') {
+        if (handled) {
+            if (event.cancelable) {
+                event.preventDefault();
+            }
+            event.stopPropagation();
+        }
+        if (event.type === upEventName || event.type === cancelEventName) {
+            if (event.type === cancelEventName) {
+                this.clearCanvas();
+            }
             this.startPosition = undefined;
             this.endPosition = undefined;
         }
